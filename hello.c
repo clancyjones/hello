091494eb7868b7ac64a5c769a74495e5f41494b7
clancyjones <111881055+clancyjones@users.noreply.github.com>
2022-08-25 14:18:04 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,13 @@
+/**
+    *Author: Clara Jones and Reagan Kahla
+    *basic hello code
+*/
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Hello World! It's meee\n");
+
+    return 0;
+}
