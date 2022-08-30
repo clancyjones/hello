2bffc730d9e2831416a94c0887ae5a5d76681a75
ReaganK4 <111812664+ReaganK4@users.noreply.github.com>
2022-08-30 16:06:29 +0000

    added Reagan name and major

--- hello.c
+++ hello.c
@@ -7,7 +7,9 @@
 
 int main(int argc, char **argv) {
 
-    printf("Hello World! It's meee\n");
+    printf("Hello World! It's Clara! I'm a computer science major!\n");
+    printf("Hi world! It's Reagan :D i'm a computer engineer!\n");
+    printf("hope this works!");
 
     return 0;
 }
